960adfc7238f67b4aa5efbb292dc1394693cc90d
Liz Liu <sky020501@163.com>
2023-06-25 19:48:48 +0800

    6.25

---
+++ src/security&authenticate/decrypt.go
@@ -0,0 +1,19 @@
+package sa
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func decrypt(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext := make([]byte, len(ciphertext))
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(plaintext, ciphertext)
+
+	return plaintext, nil
+}
---
+++ src/security&authenticate/encrypt.go
@@ -0,0 +1,19 @@
+package sa
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+)
+
+func encrypt(plaintext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	ciphertext := make([]byte, len(plaintext))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, plaintext)
+
+	return ciphertext, nil
+}
